ecbbce395ea549ad2e5e03eb6138cfc2c475403b
Lo Van Long <longlv@v-auto.com.vn>
2021-07-06 15:23:21 +0700

    generate

---
+++ components/FormLayouts.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+  <div class="md:grid md:grid-cols-3 md:gap-6">
+    <div class="md:col-span-1">
+      <div class="px-4 sm:px-0">
+        <h3 class="text-lg font-medium leading-6 text-gray-900">Đăng ký</h3>
+        <p class="mt-1 text-sm text-gray-600">
+          Sau khi nhận được thông tin đăng ký ứng tuyển của bạn, chúng tôi sẽ liên hệ với bạn để hỗ trợ bạn tìm công việc phù hợp.
+        </p>
+      </div>
+    </div>
+    <div class="mt-5 md:mt-0 md:col-span-2">
+      <!-- Begin Mailchimp Signup Form -->
+<div id="mc_embed_signup">
+<form id="mc-embedded-subscribe-form" action="https://vieclambaove.us1.list-manage.com/subscribe/post?u=dafd0f159bea8e5be932061e6&amp;id=a4581e997f" method="post" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div class="shadow sm:rounded-md sm:overflow-hidden">
+
+    
+    <div id="mc_embed_signup_scroll" class="px-4 py-5 bg-white space-y-6 sm:p-6">
+<div>
+	<label class="block text-sm font-medium text-gray-700" for="mce-FNAME">Họ tên  <span class="asterisk">*</span>
+</label>
+	<div class="mt-1">
+    <input id="mce-FNAME" type="text" value="" name="FNAME" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300">
+    </div>
+</div>
+<div>
+	<label  class="block text-sm font-medium text-gray-700" for="mce-YEAROLD">Tuổi  <span class="asterisk">*</span>
+</label>
+	<div class="mt-1">
+    <input id="mce-YEAROLD" type="number" name="YEAROLD" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" value="">
+    </div>
+</div>
+<div>
+	<label  class="block text-sm font-medium text-gray-700" for="mce-BMI">Chiều cao + cân nặng  <span class="asterisk">*</span>
+</label>
+	<div class="mt-1">
+    <input id="mce-BMI" type="text" value="" name="BMI" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300">
+    </div>
+</div>
+<div>
+	<label  class="block text-sm font-medium text-gray-700" for="mce-PHONE">Số điện thoại  <span class="asterisk">*</span>
+</label>
+	<div class="mt-1">
+    <input id="mce-PHONE" type="text" name="PHONE" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" value="">
+    </div>
+</div>
+<div>
+	<label  class="block text-sm font-medium text-gray-700" for="mce-CURRENTADD">Địa chỉ hiện tại  <span class="asterisk">*</span>
+</label>
+	<div class="mt-1">
+    <input id="mce-CURRENTADD" type="text" value="" name="CURRENTADD" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300">
+    </div>
+</div>
+<div>
+	<label  class="block text-sm font-medium text-gray-700" for="mce-EMAIL">Email  <span class="asterisk">*</span>
+</label>
+	<div class="mt-1">
+    <input id="mce-EMAIL" type="email" value="" name="EMAIL" class="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300">
+    </div>
+</div>
+	<div id="mce-responses" class="clear">
+		<div id="mce-error-response" class="response" style="display:none"></div>
+		<div id="mce-success-response" class="response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_dafd0f159bea8e5be932061e6_a4581e997f" tabindex="-1" value=""></div>
+    <div class="px-4 py-3 bg-gray-50 text-right sm:px-6"><input id="mc-embedded-subscribe" type="submit" value="Ứng tuyển" name="subscribe" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"></div>
+    </div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
+    </div>
+  </div>
+</div>
+</template>
